5e107c713e1eff54f82e15823aff8a18e908a71c
Tony Edgecombe <tonyedgecombe@gmail.com>
2016-09-24 20:19:40 +0100

    Removed unused using statements

--- Sodukuk/Program.cs
+++ Sodukuk/Program.cs
@@ -1,8 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Linq;
-using System.Runtime.CompilerServices;
 
 namespace Sodukuk
 {
