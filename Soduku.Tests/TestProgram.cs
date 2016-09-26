c806b9395dc8f8f0e49f0025a42e4ecf1155ed87
Tony Edgecombe <tonyedgecombe@gmail.com>
2016-09-26 07:36:59 +0100

    Eliminated static methods

--- Soduku.Tests/TestProgram.cs
+++ Soduku.Tests/TestProgram.cs
@@ -10,9 +10,9 @@ namespace Soduku.Tests
         [Test]
         public void TestIsSequenceValid()
         {
-            Assert.That(Program.IsSequenceValid(new []{0, 0, 0, 0, 0, 0, 0, 0, 0}), Is.True);
-            Assert.That(Program.IsSequenceValid(new []{1, 2, 3, 4, 5, 6, 7, 8, 9}), Is.True);
-            Assert.That(Program.IsSequenceValid(new []{1, 2, 3, 4, 5, 6, 7, 9, 9}), Is.False);
+            Assert.That(new Program().IsSequenceValid(new []{0, 0, 0, 0, 0, 0, 0, 0, 0}), Is.True);
+            Assert.That(new Program().IsSequenceValid(new []{1, 2, 3, 4, 5, 6, 7, 8, 9}), Is.True);
+            Assert.That(new Program().IsSequenceValid(new []{1, 2, 3, 4, 5, 6, 7, 9, 9}), Is.False);
         }
 
         [Test]
@@ -20,9 +20,9 @@ namespace Soduku.Tests
         {
             var arr = Enumerable.Range(0, 81).ToArray();
 
-            Assert.That(Program.GetRow(arr, 0), Is.EqualTo(Enumerable.Range(0, 9).ToArray()));
-            Assert.That(Program.GetRow(arr, 1), Is.EqualTo(Enumerable.Range(9, 9).ToArray()));
-            Assert.That(Program.GetRow(arr, 8), Is.EqualTo(Enumerable.Range(72, 9).ToArray()));
+            Assert.That(new Program().GetRow(arr, 0), Is.EqualTo(Enumerable.Range(0, 9).ToArray()));
+            Assert.That(new Program().GetRow(arr, 1), Is.EqualTo(Enumerable.Range(9, 9).ToArray()));
+            Assert.That(new Program().GetRow(arr, 8), Is.EqualTo(Enumerable.Range(72, 9).ToArray()));
         }
 
         [Test]
@@ -30,7 +30,7 @@ namespace Soduku.Tests
         {
             var arr = Enumerable.Range(0, 81).ToArray();
             arr[0] = 1;
-            Assert.That(Program.GetColumn(arr, 0), Is.EqualTo(new []{1, 9, 18, 27, 36, 45, 54, 63, 72}));
+            Assert.That(new Program().GetColumn(arr, 0), Is.EqualTo(new []{1, 9, 18, 27, 36, 45, 54, 63, 72}));
         }
 
         [Test]
@@ -38,13 +38,13 @@ namespace Soduku.Tests
         {
             var arr = new int[81];
 
-            Assert.That(Program.AreRowsValid(arr), Is.True);
+            Assert.That(new Program().AreRowsValid(arr), Is.True);
             Enumerable.Range(0, 9).ToArray().CopyTo(arr, 0);
 
-            Assert.That(Program.AreRowsValid(arr), Is.True);
+            Assert.That(new Program().AreRowsValid(arr), Is.True);
 
             arr[0] = 1;
-            Assert.That(Program.AreRowsValid(arr), Is.False);
+            Assert.That(new Program().AreRowsValid(arr), Is.False);
         }
 
         [Test]
@@ -52,21 +52,21 @@ namespace Soduku.Tests
         {
             var arr = new int[81];
 
-            Assert.That(Program.IsRowValid(arr, 0), Is.True);
+            Assert.That(new Program().IsRowValid(arr, 0), Is.True);
             Enumerable.Range(0, 9).ToArray().CopyTo(arr, 0);
 
             arr[0] = 1;
-            Assert.That(Program.IsRowValid(arr, 0), Is.False);
+            Assert.That(new Program().IsRowValid(arr, 0), Is.False);
         }
 
         [Test]
         public void TestIsColumnValid()
         {
             var arr = Enumerable.Range(0, 81).ToArray();
-            Assert.That(Program.IsColumnValid(arr, 0), Is.True);
+            Assert.That(new Program().IsColumnValid(arr, 0), Is.True);
 
             arr[0] = 9;
-            Assert.That(Program.IsColumnValid(arr, 0), Is.False);
+            Assert.That(new Program().IsColumnValid(arr, 0), Is.False);
         }
 
         [Test]
@@ -74,23 +74,23 @@ namespace Soduku.Tests
         {
             var arr = Enumerable.Range(0, 81).ToArray();
 
-            Assert.That(Program.AreColumnsValid(arr), Is.True);
+            Assert.That(new Program().AreColumnsValid(arr), Is.True);
 
             arr[0] = 9;
-            Assert.That(Program.AreColumnsValid(arr), Is.False);
+            Assert.That(new Program().AreColumnsValid(arr), Is.False);
         }
 
         [Test]
         public void TestGetNextTestCell()
         {
             var arr = new int[81];
-            Assert.That(Program.GetNextTestCell(arr), Is.EqualTo(0));
+            Assert.That(new Program().GetNextTestCell(arr), Is.EqualTo(0));
 
             Enumerable.Range(0, 27).Select(c => 1).ToArray().CopyTo(arr, 0);
-            Assert.That(Program.GetNextTestCell(arr), Is.EqualTo(27));
+            Assert.That(new Program().GetNextTestCell(arr), Is.EqualTo(27));
 
             Enumerable.Range(0, 81).Select(c => 1).ToArray().CopyTo(arr, 0);
-            Assert.That(Program.GetNextTestCell(arr), Is.EqualTo(81));
+            Assert.That(new Program().GetNextTestCell(arr), Is.EqualTo(81));
         }
 
         [Test]
@@ -98,17 +98,17 @@ namespace Soduku.Tests
         {
             var arr = Enumerable.Range(0, 81).ToArray();
 
-            Assert.That(Program.GetBlock(arr, 0, 0), Is.EqualTo(new[] {0, 1, 2, 9, 10, 11, 18, 19, 20}));
-            Assert.That(Program.GetBlock(arr, 0, 1), Is.EqualTo(new[] {3, 4, 5, 12, 13, 14, 21, 22, 23}));
-            Assert.That(Program.GetBlock(arr, 0, 2), Is.EqualTo(new[] {6, 7, 8, 15, 16, 17, 24, 25, 26}));
+            Assert.That(new Program().GetBlock(arr, 0, 0), Is.EqualTo(new[] {0, 1, 2, 9, 10, 11, 18, 19, 20}));
+            Assert.That(new Program().GetBlock(arr, 0, 1), Is.EqualTo(new[] {3, 4, 5, 12, 13, 14, 21, 22, 23}));
+            Assert.That(new Program().GetBlock(arr, 0, 2), Is.EqualTo(new[] {6, 7, 8, 15, 16, 17, 24, 25, 26}));
 
-            Assert.That(Program.GetBlock(arr, 1, 0), Is.EqualTo(new[] {27, 28, 29, 36, 37, 38, 45, 46, 47}));
-            Assert.That(Program.GetBlock(arr, 1, 1), Is.EqualTo(new[] {30, 31, 32, 39, 40, 41, 48, 49, 50}));
-            Assert.That(Program.GetBlock(arr, 1, 2), Is.EqualTo(new[] {33, 34, 35, 42, 43, 44, 51, 52, 53}));
+            Assert.That(new Program().GetBlock(arr, 1, 0), Is.EqualTo(new[] {27, 28, 29, 36, 37, 38, 45, 46, 47}));
+            Assert.That(new Program().GetBlock(arr, 1, 1), Is.EqualTo(new[] {30, 31, 32, 39, 40, 41, 48, 49, 50}));
+            Assert.That(new Program().GetBlock(arr, 1, 2), Is.EqualTo(new[] {33, 34, 35, 42, 43, 44, 51, 52, 53}));
 
-            Assert.That(Program.GetBlock(arr, 2, 0), Is.EqualTo(new[] {54, 55, 56, 63, 64, 65, 72, 73, 74}));
-            Assert.That(Program.GetBlock(arr, 2, 1), Is.EqualTo(new[] {57, 58, 59, 66, 67, 68, 75, 76, 77}));
-            Assert.That(Program.GetBlock(arr, 2, 2), Is.EqualTo(new[] {60, 61, 62, 69, 70, 71, 78, 79, 80}));
+            Assert.That(new Program().GetBlock(arr, 2, 0), Is.EqualTo(new[] {54, 55, 56, 63, 64, 65, 72, 73, 74}));
+            Assert.That(new Program().GetBlock(arr, 2, 1), Is.EqualTo(new[] {57, 58, 59, 66, 67, 68, 75, 76, 77}));
+            Assert.That(new Program().GetBlock(arr, 2, 2), Is.EqualTo(new[] {60, 61, 62, 69, 70, 71, 78, 79, 80}));
         }
 
         [Test]
@@ -129,9 +129,9 @@ namespace Soduku.Tests
                         }
                     }
 
-                    Assert.That(Program.IsBlockValid(arr, rb, cb), Is.True);
+                    Assert.That(new Program().IsBlockValid(arr, rb, cb), Is.True);
                     arr[pos] = 2;
-                    Assert.That(Program.IsBlockValid(arr, rb, cb), Is.False);
+                    Assert.That(new Program().IsBlockValid(arr, rb, cb), Is.False);
                 }
             }
         }
@@ -141,20 +141,20 @@ namespace Soduku.Tests
         {
             var arr = Enumerable.Range(0, 81).ToArray();
 
-            Assert.That(Program.AreBlocksValid(arr), Is.True);
+            Assert.That(new Program().AreBlocksValid(arr), Is.True);
 
             arr[0] = 1;
-            Assert.That(Program.AreBlocksValid(arr), Is.False);
+            Assert.That(new Program().AreBlocksValid(arr), Is.False);
         }
         [Test]
         public void TestIsValid()
         {
             var arr = Enumerable.Range(0, 81).ToArray();
 
-            Assert.That(Program.IsValid(arr), Is.True);
+            Assert.That(new Program().IsValid(arr), Is.True);
 
             arr[0] = 1;
-            Assert.That(Program.IsValid(arr), Is.False);
+            Assert.That(new Program().IsValid(arr), Is.False);
         }
     }
 }
--- Soduku/Program.cs
+++ Soduku/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 
 namespace Soduku
 {
-    public static class Program
+    public class Program
     {
         static void Main()
         {
@@ -22,20 +22,20 @@ namespace Soduku
                 0, 0, 0, 0, 0, 0, 0, 0, 0,
             };
 
-            var solution = Solve(arr);
+            var solution = new Program().Solve(arr);
 
             Console.WriteLine(string.Join("\n", Enumerable.Range(0, 9).Select(
                 row => string.Join(",", solution.Skip(9*row).Take(9))
             )));
         }
 
-        public static int[] Solve(int[] grid)
+        public int[] Solve(int[] grid)
         {
             var nextCell = GetNextTestCell(grid);
             return nextCell == 81 ? grid : SolveForCell(grid, nextCell);
         }
 
-        private static int[] SolveForCell(int[] grid, int cell)
+        private int[] SolveForCell(int[] grid, int cell)
         {
             return Enumerable.Range(1, 9)
                 .Select(i => UpdateElement(grid, cell, i))
@@ -44,32 +44,32 @@ namespace Soduku
                 .FirstOrDefault(solution => solution != null);
         }
 
-        private static int[] UpdateElement(int[] grid, int cell, int value)
+        private int[] UpdateElement(int[] grid, int cell, int value)
         {
             return Enumerable.Range(0, 81).Select(i => i == cell ? value : grid[i]).ToArray();
         }
 
-        public static int GetNextTestCell(int[] grid)
+        public int GetNextTestCell(int[] grid)
         {
             return Enumerable.Range(0, 82).TakeWhile(i => i < 81 && grid[i] != 0).DefaultIfEmpty(-1).Last() + 1;
         }
 
-        public static bool IsValid(int[] grid)
+        public bool IsValid(int[] grid)
         {
             return AreColumnsValid(grid) && AreRowsValid(grid) && AreBlocksValid(grid);
         }
 
-        public static bool AreBlocksValid(int[] grid)
+        public bool AreBlocksValid(int[] grid)
         {
             return Enumerable.Range(0, 3).All(rb => Enumerable.Range(0, 3).All(cb => IsBlockValid(grid, rb, cb)));
         }
 
-        public static bool IsBlockValid(int[] grid, int rb, int cb)
+        public bool IsBlockValid(int[] grid, int rb, int cb)
         {
             return IsSequenceValid(GetBlock(grid, rb, cb).ToArray());
         }
 
-        public static int[] GetBlock(int[] grid, int rb, int cb)
+        public int[] GetBlock(int[] grid, int rb, int cb)
         {
             return Enumerable.Range(0, 3).Select(column =>
                     Enumerable.Range(0, 3).Select(row => grid[(rb*3*9) + row + (cb*3) + column*9]))
@@ -78,37 +78,37 @@ namespace Soduku
         }
 
 
-        public static bool AreRowsValid(int[] grid)
+        public bool AreRowsValid(int[] grid)
         {
             return Enumerable.Range(0, 9).All(row => IsRowValid(grid, row));
         }
 
-        public static bool AreColumnsValid(int[] grid)
+        public bool AreColumnsValid(int[] grid)
         {
             return Enumerable.Range(0, 9).All(column => IsColumnValid(grid, column));
         }
 
-        public static bool IsColumnValid(int[] grid, int column)
+        public bool IsColumnValid(int[] grid, int column)
         {
             return IsSequenceValid(GetColumn(grid, column));
         }
 
-        public static bool IsRowValid(IEnumerable<int> grid, int row)
+        public bool IsRowValid(IEnumerable<int> grid, int row)
         {
             return IsSequenceValid(GetRow(grid, row));
         }
 
-        public static int[] GetRow(IEnumerable<int> grid, int row)
+        public int[] GetRow(IEnumerable<int> grid, int row)
         {
             return grid.Skip(row*9).Take(9).ToArray();
         }
 
-        public static int[] GetColumn(int[] grid, int column)
+        public int[] GetColumn(int[] grid, int column)
         {
             return Enumerable.Range(0, 9).Select(r => grid[column + r*9]).ToArray();
         }
 
-        public static bool IsSequenceValid(IEnumerable<int> row)
+        public bool IsSequenceValid(IEnumerable<int> row)
         {
             return row.Where(c => c != 0).GroupBy(c => c).All(g => g.Count() == 1);
         }
